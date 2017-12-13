1b9034bf75b783c2f455aa9c7374c02ad283ed75
Ramanujan Uppili <ramanujanuppili@Ramanujans-MacBook-Pro.local>
2017-12-13 13:13:06 +0530

    initial commit

---
+++ src/main/java/com/fileshare/FilesShareApp/FilesShareAppApplication.java
@@ -0,0 +1,17 @@
+package com.fileshare.FilesShareApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class FilesShareAppApplication  {
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilesShareAppApplication.class, args);
+	}
+}
